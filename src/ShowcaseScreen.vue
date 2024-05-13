8c4975af63cff9f826f474d0ddd5e1b1426ba4b3
yhsphd <yhsphd05@gmail.com>
2024-05-13 17:10:47 +0900

    Showcase: Show mappool list insted of just index

--- src/ShowcaseScreen.vue
+++ src/ShowcaseScreen.vue
@@ -4,26 +4,12 @@ import LogoHeader from "@/components/LogoHeader.vue";
 import CurrentMap from "@/components/CurrentMap.vue";
 import { computed, ref } from "vue";
 import DimensionInfo from "@/components/DimensionInfo.vue";
+import MappoolView from "@/components/MappoolView.vue";
 
 const state = useOverlayDataStore();
 socket.off(); // remove any existing listeners (after a hot module replacement)
 state.bindEvents();
 state.connect();
-
-const totalMapCount = computed(() => state.data.mappool?.length);
-const mapIndex = computed(() => {
-  if (!state.data?.mappool?.length) {
-    return 0;
-  }
-
-  for (let i = 0; i < state.data.mappool.length; i++) {
-    if (state.data.now_playing.osu.code === state.data.mappool[i].code) {
-      return i + 1;
-    }
-  }
-
-  return 0;
-});
 
 const contentBg_ref = ref(null);
 </script>
@@ -41,10 +27,8 @@ const contentBg_ref = ref(null);
       <div class="headerWrapper">
         <logo-header class="header" orientation="vertical"></logo-header>
       </div>
-      <div class="mapIndexWrapper">
-        <div class="mapIndex" :style="{ opacity: mapIndex ? 1 : 0 }">
-          Map <b>{{ mapIndex }}</b> of {{ totalMapCount }}
-        </div>
+      <div class="mappoolWrapper">
+        <mappool-view class="mappool"></mappool-view>
       </div>
       <div class="nowPlayingWrapper">
         <current-map class="nowPlaying"></current-map>
@@ -83,20 +67,6 @@ const contentBg_ref = ref(null);
   left: 40px;
 }
 
-.mapIndexWrapper {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-}
-
-.mapIndex {
-  position: absolute;
-  bottom: 20px;
-  left: 40px;
-  font-size: 24px;
-  transition: opacity 1s;
-}
-
 .nowPlayingWrapper {
   position: absolute;
   width: 100%;
@@ -108,6 +78,20 @@ const contentBg_ref = ref(null);
   top: 40px;
   left: 50%;
   transform: translateX(-50%);
+}
+
+.mappoolWrapper {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+
+.mappool {
+  position: absolute;
+  bottom: 20px;
+  left: 40px;
+  font-size: 24px;
+  transition: opacity 1s;
 }
 
 .dimensions {
